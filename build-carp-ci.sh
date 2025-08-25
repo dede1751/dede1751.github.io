effc5d176980584e0c25b3dc0d1ed6e37fa836f2
dede1751 <63297263+dede1751@users.noreply.github.com>
2025-08-25 23:24:10 +0200

    Fix broken wasm-pack link

--- build-carp-ci.sh
+++ build-carp-ci.sh
@@ -5,7 +5,7 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
 echo "Installing wasm-pack..."
-curl https://drager.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 
 echo "Building carp-wasm..."
 ./build-carp.sh
